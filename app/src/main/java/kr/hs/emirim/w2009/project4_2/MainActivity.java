33f013deec2f48ee489febe35e7b1d6f60ee283d
OHMINJUNG <w2009@e-mirim.hs.kr>
2021-04-15 19:28:27 +0900

    직접풀어보기 4-4 선택완료버튼 없애기

--- app/src/main/java/kr/hs/emirim/w2009/project4_2/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2009/project4_2/MainActivity.java
@@ -29,10 +29,9 @@ public class MainActivity extends AppCompatActivity {
         linear=findViewById(R.id.linear);
         rg=findViewById(R.id.rg);
         imgv = findViewById(R.id.imgv);
-        Button btnDone=findViewById(R.id.btn_done);
 
         switchStart.setOnCheckedChangeListener(checkListener);
-        btnDone.setOnClickListener(btnListener);
+        rg.setOnCheckedChangeListener(rgListener);
 
 
     }
@@ -47,14 +46,13 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
-    View.OnClickListener btnListener=new View.OnClickListener() {
+    RadioGroup.OnCheckedChangeListener rgListener = new RadioGroup.OnCheckedChangeListener() {
         @Override
-        public void onClick(View v) {
-            switch(rg.getCheckedRadioButtonId()){
-                case R.id.radio_dog : imgv.setImageResource(R.drawable.dog); break;
-                case R.id.radio_cat : imgv.setImageResource(R.drawable.cat); break;
-                case R.id.radio_rabbit : imgv.setImageResource(R.drawable.rabbit); break;
-
+        public void onCheckedChanged(RadioGroup group, int checkedId) {
+            switch(checkedId){
+                case R.id.radio_dog:imgv.setImageResource(R.drawable.dog);break;
+                case R.id.radio_cat:imgv.setImageResource(R.drawable.cat);break;
+                case R.id.radio_rabbit:imgv.setImageResource(R.drawable.rabbit);break;
             }
         }
     };
